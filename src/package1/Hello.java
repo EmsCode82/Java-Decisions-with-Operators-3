a8d2886e2f8c40c9be622bead710579e50bb42c4
EmsCode82 <emsgithub@gmail.com>
2021-11-09 15:07:34 -0600

    .

---
+++ src/package1/Hello.java
@@ -0,0 +1,31 @@
+package package1;
+public class Hello {
+
+	public static void main(String[] args) {
+		/*
+		int x = 5;
+		int y = 10;
+		//boolean x = true;//
+		//boolean y = false;//
+		
+		if(!(x < y) || (x == y)) {
+			System.out.println("Condition is True");
+		}else {
+			System.out.println("Condition is False");
+		}
+	*/
+		
+		int ageOfBoy = 39;
+		int ageOfGirl = 24;
+		
+		if((ageOfBoy >= 21) && (ageOfGirl >= 18)) {
+			System.out.println("ready to get married");
+		}else {
+			System.out.println("Wait for it kiddo!");
+		}
+		
+		
+		
+	}
+
+}
